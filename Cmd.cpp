221e0731b06ab54f23a9270dfa8c7e122f08c90a
Mojszli <viktor.denes2001@gmail.com>
2025-06-11 16:43:12 +0200

    Add files via upload

---
+++ Cmd.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    std::string input;
+    std::cout << "Enter a command: ";
+    std::getline(std::cin, input);
+    if(input == "ipconfig") {
+        system("ipconfig");
+    }
+    
+    else if(input == "dir") {
+        system("dir");
+    }
+
+    else if(input == "cls") {
+        system("cls");
+    }
+
+    else if(input == "exit") {
+        std::cout << "Exiting..." << std::endl;
+        return 0;
+    }
+
+    else {
+        std::cout << "Unknown command: " << input << std::endl;
+    }
+
+
+    return 0;
+}
